ce71a7ac3913dc4e00334e4240b13822475de9f1
first-test-repo <Mukul.Mahapatra@366PiTech.com>
2025-08-29 13:49:02 +0530

    hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,19 @@
-# simple script
-print("Hello, world from Git!")
-print("Hello, Mukul!")
+#Armstrong Number script
+
+
+# Armstrong number checker
+
+# Take input from user
+num = int(input("Enter a number: "))
+
+# Convert number to string to count digits
+order = len(str(num))
+
+# Compute sum of digits raised to the power
+sum_of_powers = sum(int(digit) ** order for digit in str(num))
+
+# Check condition
+if num == sum_of_powers:
+    print(f"{num} is an Armstrong number.")
+else:
+    print(f"{num} is NOT an Armstrong number.")
